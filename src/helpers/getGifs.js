2ed14f1e81938a30ac421f40ccac02a84290b01d
Sed2295 <sed.silver@hotmail.com>
2021-04-15 12:51:53 -0500

    arreglo de seguridad en helpers

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async (category) => {
-    const url = `http://api.giphy.com/v1/gifs/search?api_key=8FZIHm9CO4uo4ARK5lflU4CY9kUTr2Pv&limit=10&q=${category}`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=8FZIHm9CO4uo4ARK5lflU4CY9kUTr2Pv&limit=10&q=${category}`;
     const res = await fetch(url);
     const {data} = await res.json();
     const gifs = data.map(img => {
